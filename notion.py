42af80c3bd934ba01a2145c1e3cc62eec8f6f6d8
GuitarGuy99 <122157280+GuitarGuy99@users.noreply.github.com>
2023-01-08 22:11:35 +0000

    Update notion.py

--- notion.py
+++ notion.py
@@ -1,11 +1,12 @@
+import re
 import requests
 
 
 def make_box_page(box_name, nmap_output):
   # Notion API Token
-  TOKEN = "{NOTION_API_KEY_HERE}"
+  TOKEN = "secret_Lvr0IgYzXQP1yrXRAzVmca3DQzG5GefTMZWaBKlg6JQ"
   # Parent page id
-  PARENT = "{PARENT_ID_OF_NOTION_PAGE_HERE}"
+  PARENT = "91c253db90d342fe9f7a44419fbe0481"
 
   url = "https://api.notion.com/v1/pages"
 
@@ -15,6 +16,25 @@ def make_box_page(box_name, nmap_output):
     "content-type": "application/json",
     "Authorization": f"Bearer {TOKEN}"
   }
+
+  code_blocks = []
+  chunk_size = 2000
+  total_size = len(nmap_output)
+  chunks_out = [ nmap_output[i:i+chunk_size] for i in range(0, total_size, chunk_size) ]
+
+  for chunk in chunks_out:
+    code_blocks.append({
+      "type": "text",
+      "text": {
+        "content": chunk
+      }
+    })
+
+  scan_details = []
+  find_matches = re.findall(r'^([0-9]+)\/(tcp|udp)\s+([^\s]+)\s+([^\s]+)\s+(.*)$', nmap_output, flags=re.MULTILINE)
+
+  for port, proto, state, service, version in find_matches:
+    scan_details.append(f"Found a {service} {version} Server {state} on {proto} {port}")
 
   payload = {
     "parent": {
@@ -51,14 +71,20 @@ def make_box_page(box_name, nmap_output):
       "object": "block",
       "type": "code",
       "code": {
+        "rich_text": code_blocks,
+        "language":
+        "plain text"
+      }
+    },{
+      "object": "block",
+      "type": "paragraph",
+      "paragraph": {
         "rich_text": [{
           "type": "text",
           "text": {
-            "content": nmap_output
+            "content": "\n".join(scan_details)
           }
-        }],
-        "language":
-        "plain text"
+        }]
       }
     }]
   }
